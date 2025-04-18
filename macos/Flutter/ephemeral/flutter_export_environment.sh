7fafa8e361926f503008f0791817427c968e8f05
Tarun Sisodia <coccoder999@gmail.com>
2025-04-18 19:25:20 +0530

    fix: Correct FLUTTER_APPLICATION_PATH and update FLUTTER_BUILD_NAME in generated configuration files

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/charon/flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/charon/Studio/smartcampus"
+export "FLUTTER_APPLICATION_PATH=/home/charon/Studio/SmartCampus"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
