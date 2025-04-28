8ba9145bc0e4a0ca97fa20442dfb435b31d57bd1
Tarun sisodia <coccoder999@gmail.com>
2025-04-28 21:25:57 +0530

    now only app will be upload

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/charon/flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/charon/Studio/SmartCampus"
+export "FLUTTER_APPLICATION_PATH=/home/charon/Studio/smartcampus"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
